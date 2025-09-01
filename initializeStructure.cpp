1959309123e2bacd30d4c522a48a4f8d2ae29f6b
fazlerabbi13 <mohammad0132794@gmail.com>
2025-09-01 23:24:59 +0600

    Initialize data members of structure using curly braces.

---
+++ initializeStructure.cpp
@@ -0,0 +1,12 @@
+struct Point {
+    int x, y;
+};
+
+int main() {
+    
+    // Initialize data members of
+    // structure using curly braces
+    struct Point p1 = { 0, 1 };
+    
+    return 0;
+}
