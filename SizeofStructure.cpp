c6f6e1ef3cc6a51c67a13e185bfd71f09f696d81
fazlerabbi13 <mohammad0132794@gmail.com>
2025-09-02 23:01:07 +0600

    The size of a structure is determined by the sum of the sizes of its individual data members, with additional padding added by the compiler to ensure proper memory alignment.

---
+++ SizeofStructure.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+struct Hello {
+    char c;
+    int x, y;
+};
+
+int main() {
+    
+    // Finding the size
+    cout << sizeof(Hello);
+    
+    return 0;
+}
